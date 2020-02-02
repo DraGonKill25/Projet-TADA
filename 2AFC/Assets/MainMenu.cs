f19b2b1f3003313e0c16cbdec7170489544da552
DraGonKill25 <adam.rili@epita.fr>
2020-02-02 01:08:10 +0100

    Debut du menu

---
+++ 2AFC/Assets/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManagement.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
