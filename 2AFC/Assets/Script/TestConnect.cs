251670fd1da48cf473042b280c97f77d10e4cf67
tanguy.maraux <tanguy.maraux@epita.fr>
2020-02-06 20:00:06 +0100

    début de photon

---
+++ 2AFC/Assets/Script/TestConnect.cs
@@ -0,0 +1,26 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestConnect : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        print("Connecting to server.");
+        PhotonNetwork.GameVersion = "0.0.1";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        print("Connected to server.");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        print("Disconnected to server for " + cause.ToString());
+    }
+        
+}
