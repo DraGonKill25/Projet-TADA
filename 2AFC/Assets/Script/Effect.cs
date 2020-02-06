b1fa9b73da2c8b937bc83651d4f675d50949dc42
DraGonKill25 <adam.rili@epita.fr>
2020-02-06 17:31:13 +0100

    script pour les classes

---
+++ 2AFC/Assets/Script/Effect.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Effect
+{
+    class Effet : Spell
+    {
+
+    }
+}
---
+++ 2AFC/Assets/Script/Sort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using classeCarac;
+using Joueur;
+
+namespace Sort
+{
+    class Spell : Player
+    {
+
+        private int mb_level;
+        public static readonly int[] MALEDICTION = { 0, 5, 10, 15, 20 };
+        public static readonly int[] BENEDICTION = { 0, 5, 10, 15, 20 };
+
+
+        public Spell()
+        {
+            this.mb_level = 0;
+        }
+
+
+        public int Mb_level
+        {
+            get { return mb_level; }
+        }
+
+
+        public int Malediciton(int timer)
+        {
+            return attaque * (1 - MALEDICTION[mb_level]);
+        }
+
+
+
+    }
+}
