a6ab414edc296d59c9e942b1ea3baf479bfb181e
tanguy.maraux <tanguy.maraux@epita.fr>
2020-03-06 18:54:22 +0100

    add Menu temp & textmesh asset

---
+++ 2AFC/Assets/Script/Menu scripts/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{    public void LaunchPlayGameMenu()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
