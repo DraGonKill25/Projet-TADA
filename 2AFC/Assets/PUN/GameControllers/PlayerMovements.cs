e40d490e78863ed6c0df20ac93b095173f7000a5
Mael-221 <60392471+Mael-221@users.noreply.github.com>
2020-03-07 16:16:55 +0100

    camera sur le perso

--- 2AFC/Assets/PUN/GameControllers/PlayerMovements.cs
+++ 2AFC/Assets/PUN/GameControllers/PlayerMovements.cs
@@ -9,16 +9,26 @@ public class PlayerMovements : MonoBehaviour
     private CharacterController myCC;
     public float movementSpeed;
     public float rotationSpeed;
+    public GameObject playercamera;
+    public Transform camera;
 
     void Start()
     {
+        Cursor.visible = false;
         PV = GetComponent<PhotonView>();
         myCC = GetComponent<CharacterController>();
+        if (PV.IsMine)
+        {
+            playercamera.SetActive(true);
+        }
+        else
+            playercamera.SetActive(false);
+
     }
 
     void Update()
     {
-        if(PV.IsMine)
+        if (PV.IsMine)
         {
             BasicMovement();
             BasicRotation();
@@ -27,7 +37,7 @@ public class PlayerMovements : MonoBehaviour
 
     void BasicMovement()
     {
-        if(Input.GetKey(KeyCode.Z))
+        if (Input.GetKey(KeyCode.Z))
         {
             myCC.Move(transform.forward * Time.deltaTime * movementSpeed);
         }
@@ -52,5 +62,20 @@ public class PlayerMovements : MonoBehaviour
     {
         float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * rotationSpeed;
         transform.Rotate(new Vector3(0, mouseX, 0));
+        
+        float mouseY = -Input.GetAxis("Mouse Y") * Time.deltaTime * rotationSpeed;
+
+         if (camera.position.y<4 && mouseY>0 )
+         {
+             camera.position += (new Vector3(0, mouseY / 15, 0));
+             camera.Rotate (new Vector3( mouseY/2,0, 0)); 
+         }
+
+         if (camera.position.y >0 && mouseY< 0 )
+         {
+             camera.position += (new Vector3(0, mouseY / 15, 0));
+             camera.Rotate(new Vector3( mouseY/2, 0,0));
+         }
     }
 }
+
