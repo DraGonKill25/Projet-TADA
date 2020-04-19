2017c9d934e1195b6e44c6849800e6ab813f3603
tanguy.maraux <tanguy.maraux@epita.fr>
2020-04-19 04:01:44 +0200

    Scenes globalement toutes reliées, jeu jouable mais encore bcp à régler

---
+++ 2AFC/Assets/Script/Menu_scripts/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Start()
+    {
+        Pause.paused = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    public void LaunchPlayGameMenu()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
