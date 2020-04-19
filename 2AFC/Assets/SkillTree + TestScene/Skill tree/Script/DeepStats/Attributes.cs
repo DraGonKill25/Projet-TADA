3c534990083451cbf26580ddc623c95b23bc94dc
Adam <adam.rili@epita.fr>
2020-04-19 02:13:13 +0200

    on remet tout sauf les scenes a Tanguy

---
+++ 2AFC/Assets/SkillTree + TestScene/Skill tree/Script/DeepStats/Attributes.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Script
+{
+    [CreateAssetMenu(menuName = "RPG generator/player/Create Attribute")]
+    public class Attributes : ScriptableObject
+    {
+        public string Description;
+        public Sprite Icon;
+    }
+}
