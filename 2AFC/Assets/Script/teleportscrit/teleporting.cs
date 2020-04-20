58206aa6fb42034c2cd8ffd17277db3e54feb623
Adam <adam.rili@epita.fr>
2020-04-20 19:26:00 +0200

    rajout de l'inventaire, arbre de competance + petit changement sur la map

---
+++ 2AFC/Assets/Script/teleportscrit/teleporting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class teleporting : MonoBehaviour
+{
+    public Transform TeleportTarget;
+    public GameObject Player;
+    
+    
+
+    void OnTriggerEnter(Collider other)
+    {
+        Photon.Realtime.Player[] test = PhotonNetwork.PlayerList;
+        int l = test.Length - 1;
+
+        Player.transform.position = TeleportTarget.transform.position;
+    }
+}
