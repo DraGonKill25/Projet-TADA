b60b90ba23b069f3256d41774e0b110c6c5229d4
Mael-221 <60392471+Mael-221@users.noreply.github.com>
2020-05-21 20:37:46 +0200

    PNJ

---
+++ 2AFC/Assets/Script/PNJ/Choixdeclasse.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Choixdeclasse : MonoBehaviour
+{
+    private bool conversation;
+    public TextMeshProUGUI TxtQuestion;
+    public TextMeshProUGUI TxtOui;
+    public TextMeshProUGUI TxtNon;
+  
+
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "boxbody")
+        {
+            TxtQuestion.gameObject.SetActive(true);
+            conversation = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "boxbody")
+        {
+            TxtQuestion.gameObject.SetActive(false);
+            conversation = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            TxtQuestion.gameObject.SetActive(false);
+            TxtOui.gameObject.SetActive(true);
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            TxtQuestion.gameObject.SetActive(false);
+            TxtNon.gameObject.SetActive(true);
+            new WaitForSeconds(2f);
+            TxtNon.gameObject.SetActive(false);
+
+        }
+    }
+}
---
+++ 2AFC/Assets/Script/PNJ/Villageois.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Villageois : MonoBehaviour
+{
+    private bool conversation;
+    public TextMeshProUGUI TxtSalut;
+    
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "boxbody")
+        {
+            TxtSalut.gameObject.SetActive(true);
+            conversation = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "boxbody")
+        {
+            TxtSalut.gameObject.SetActive(false);
+            conversation = false;
+        }
+    }
+   
+}
+    // Update is called once per frame
+   
