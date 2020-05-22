c543d204a3174f84652f57d85c64f5caf72b0d45
tanguy.maraux <tanguy.maraux@epita.fr>
2020-05-22 02:49:49 +0200

    fix Settings problem and add map to Main_menu

---
+++ 2AFC/Assets/Script/Menu_scripts/Settings/ManagerSettings.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class ManagerSettings : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioMixer.SetFloat("mastervolume", PlayerPrefs.GetFloat("mastervolume"));
+        audioMixer.SetFloat("musicvolume", PlayerPrefs.GetFloat("musicvolume"));
+        audioMixer.SetFloat("Environmentvolume", PlayerPrefs.GetFloat("Environmentvolume"));
+
+        Screen.fullScreen = (PlayerPrefs.GetInt("FullScreen") == 1);
+        Screen.SetResolution(PlayerPrefs.GetInt("ResolutionWidth"), PlayerPrefs.GetInt("ResolutionHeight"), Screen.fullScreen);
+        QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("QualityIndex"));
+    }
+}
