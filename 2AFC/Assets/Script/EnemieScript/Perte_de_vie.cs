cdb213a639aa439e3434098a1ab29320c7da555e
Adam <adam.rili@epita.fr>
2020-05-23 01:58:24 +0200

    combat avec ennemi zombie de base je fais les boss demain

---
+++ 2AFC/Assets/Script/EnemieScript/Perte_de_vie.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perte_de_vie : MonoBehaviour
+{
+    private int ZombiePV;
+    TextMesh myText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myText = GetComponent<TextMesh>();
+        ZombiePV = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myText.text = "" + ZombiePV;
+    }
+
+    public void LooseHealth(int h)
+    {
+        ZombiePV -= h;
+    }
+
+    public bool IsZombieDead()
+    {
+        if(ZombiePV <= 0)
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ 2AFC/Assets/Script/Player Script/ColliderEpee.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderEpee : MonoBehaviour
+{
+    public GameObject myPlayer;
+    Animator anim;
+    Perte_de_vie Vie_Enemie;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = (Animator)myPlayer.GetComponent<Animator>();
+        Vie_Enemie = (Perte_de_vie)FindObjectOfType(typeof(Perte_de_vie));
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "ColliderHead")
+        {
+            AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(1);
+            if(info.IsName("Secondary_attack") || info.IsName("Third_Attack"))
+            {
+                Vie_Enemie.LooseHealth(12);
+            }
+        }
+        else if(other.gameObject.name == "ColliderBody")
+        {
+            AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(1);
+            if (info.IsName("Secondary_attack") || info.IsName("Third_Attack"))
+            {
+                Vie_Enemie.LooseHealth(7);
+            }
+        }
+    }
+}
