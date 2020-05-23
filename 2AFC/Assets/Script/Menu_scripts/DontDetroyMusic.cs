691a10b6c56b366686d91f0929d0177ff96eaf43
tanguy.maraux <tanguy.maraux@epita.fr>
2020-05-23 02:05:32 +0200

    add music keep playing between scenes

---
+++ 2AFC/Assets/Script/Menu_scripts/DontDetroyMusic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DontDetroyMusic : MonoBehaviour
+{
+    public Scene multi;
+
+    private void Awake()
+    {
+        Scene currScene = SceneManager.GetActiveScene();
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+        if (objs.Length > 1 || (currScene == multi))
+            Destroy(this.gameObject);
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
