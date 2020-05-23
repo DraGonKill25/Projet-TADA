a5f4f89b5b47b7fab12e415331a59402ffd73029
tanguy.maraux <tanguy.maraux@epita.fr>
2020-05-23 23:06:11 +0200

    Fix Keybinds settings bug

--- 2AFC/Assets/Script/Menu_scripts/Settings/ManagerSettings.cs
+++ 2AFC/Assets/Script/Menu_scripts/Settings/ManagerSettings.cs
@@ -10,7 +10,21 @@ public class ManagerSettings : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        KeybindManager.MyInstance.SaveKeys();
+        KeybindManager.MyInstance.BindKey("Forward", KeyCode.Z);
+        KeybindManager.MyInstance.BindKey("Back", KeyCode.S);
+        KeybindManager.MyInstance.BindKey("Left", KeyCode.Q);
+        KeybindManager.MyInstance.BindKey("Right", KeyCode.D);
+        KeybindManager.MyInstance.BindKey("Jump", KeyCode.Space);
+        KeybindManager.MyInstance.BindKey("Pause", KeyCode.Escape);
+        KeybindManager.MyInstance.BindKey("SkillTree", KeyCode.Tab);
+        KeybindManager.MyInstance.BindKey("Cursor", KeyCode.M);
+        KeybindManager.MyInstance.BindKey("Run", KeyCode.LeftShift);
+        KeybindManager.MyInstance.BindKey("Action_1", KeyCode.A);
+        KeybindManager.MyInstance.BindKey("Action_2", KeyCode.E);
+        KeybindManager.MyInstance.BindKey("Action_3", KeyCode.R);
+        KeybindManager.MyInstance.BindKey("Action_4", KeyCode.F);
+        KeybindManager.MyInstance.BindKey("Accept", KeyCode.O);
+        KeybindManager.MyInstance.BindKey("Refuse", KeyCode.N);
 
         audioMixer.SetFloat("mastervolume", PlayerPrefs.GetFloat("mastervolume"));
         audioMixer.SetFloat("musicvolume", PlayerPrefs.GetFloat("musicvolume"));
