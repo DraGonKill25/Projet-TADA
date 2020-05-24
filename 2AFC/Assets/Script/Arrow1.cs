7d84bfacbef5fa456ab3ec7ed48a46709bc3a879
Mael-221 <60392471+Mael-221@users.noreply.github.com>
2020-05-24 23:42:58 +0200

    tir à l'arc

---
+++ 2AFC/Assets/Script/Arrow1.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow1 : MonoBehaviour
+{
+    Rigidbody mybody;
+    GameObject player;
+    private float lifetimer = 3f;
+    private float timer;
+    private bool hit = false;
+
+    void Start()
+    {
+        mybody = GetComponent<Rigidbody>();
+        transform.rotation = Quaternion.LookRotation(mybody.velocity);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (hit && timer >= lifetimer)
+        {
+            Destroy(gameObject);
+        }
+
+        if (!hit)
+        {
+            transform.rotation = Quaternion.LookRotation(mybody.velocity);
+        }
+
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        
+        
+
+        
+        
+        if (other.collider.tag != "Arrow" && other.collider.tag != "Player")
+        {
+           
+            hit = true;
+            Stick();
+        }
+
+    }
+    private void Stick()
+    {
+        mybody.constraints = RigidbodyConstraints.FreezeAll;
+    }
+}
