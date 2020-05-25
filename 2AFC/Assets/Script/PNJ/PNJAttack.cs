6dd33d9a79bedd72a02f5d9afccda2b5c86fcc3c
Adam <adam.rili@epita.fr>
2020-05-25 09:40:39 +0200

    changement dans le player

---
+++ 2AFC/Assets/Script/PNJ/PNJAttack.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PNJAttack : MonoBehaviour
+{
+    Animator AnimPNJ;
+
+    // Update is called once per frame
+    void Update()
+    {
+        int toto = Random.Range(1, 3);
+
+        AnimPNJ.SetInteger("Attack", toto);
+    }
+}
